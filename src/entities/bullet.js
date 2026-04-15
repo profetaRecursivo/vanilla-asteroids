d63c25828f6295b58f3c20a0e4502aa418b1fe09
ArvirzuzztS <virzuzz12345@gmail.com>
2026-04-15 01:19:37 -0400

    fix: se arreglo el movimiento de la nave y se cambio la bullet a un dibujo de pixeles

--- src/entities/bullet.js
+++ src/entities/bullet.js
@@ -6,14 +6,7 @@ export default class Bullet {
     this.vy = vy;
     this.radius = 4;
     this.alive  = true;
- 
-    this.sprite = this._loadSprite();
-  }
- 
-  _loadSprite() {
-    const img = new Image();
-    img.src = "assets/sprites/bullet/bullet.png";
-    return img;
+    this.pixelSize = 2;
   }
  
   update(dt, worldWidth, worldHeight) {
@@ -32,9 +25,30 @@ export default class Bullet {
  
   draw(ctx) {
     if (!this.alive) return;
-    const d = this.radius * 2;
+    
     ctx.save();
-    ctx.drawImage(this.sprite, this.x - this.radius, this.y - this.radius, d, d);
+    ctx.fillStyle = "#ffffff";
+
+    const pattern = [
+      [false, true, false],
+      [true, true, true],
+      [false, true, false]
+    ];
+    
+    const pixelSize = this.pixelSize;
+    const startX = this.x - (3 * pixelSize) / 2;
+    const startY = this.y - (3 * pixelSize) / 2;
+    
+    for (let row = 0; row < pattern.length; row++) {
+      for (let col = 0; col < pattern[row].length; col++) {
+        if (pattern[row][col]) {
+          const px = startX + col * pixelSize;
+          const py = startY + row * pixelSize;
+          ctx.fillRect(px, py, pixelSize, pixelSize);
+        }
+      }
+    }
+    
     ctx.restore();
   }
 }
