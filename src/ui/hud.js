76cdc91189ac796a8e758679ce3d13f929e80d63
Jaime Sebastian Chavarria Fuertes <jschavarria77@gmail.com>
2026-04-22 13:17:23 -0400

    feat: frontend view of hud

--- src/ui/hud.js
+++ src/ui/hud.js
@@ -1,4 +1,24 @@
+export function updateLives() {
+  const lives = document.getElementById("lives-value");
 
-export function updateHud(){
-  
+  let value = parseInt(lives.textContent);
+  lives.textContent = value - 1;
+}
+export function updateAsteroids(size) {
+  const asteroidsValue = document.getElementById("asteroids-value");
+  let value = parseInt(asteroidsValue.textContent);
+  asteroidsValue.textContent = size;
+}
+export function updateScore(delta) {
+  const scoreValue = document.getElementById("score-value");
+  let value = parseInt(scoreValue.textContent);
+  scoreValue.textContent = fillWithCeros(value  + delta);
+}
+
+function fillWithCeros(value){
+  let ans = value + "";
+  while(ans.length < 6){
+    ans = "0" + ans;
+  }
+  return ans;
 }
